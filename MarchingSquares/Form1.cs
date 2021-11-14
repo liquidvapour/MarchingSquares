25a81f22607d7d9c67cf9e9e914734b53a2bfaf8
liquidvapour <liquidvapour@gmail.com>
2021-11-14 03:29:23 +0000

    simple implementation

--- MarchingSquares/Form1.cs
+++ MarchingSquares/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MarchingSquares
@@ -24,18 +18,140 @@ namespace MarchingSquares
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            e.Graphics.DrawArc(Pens.Black, r, 0.0f, 360.0f);
+            //e.Graphics.DrawArc(Pens.Black, rect, 0.0f, 360.0f);
+            for (int y = 0; y < ClientSize.Height; y+=squareSize)
+            {
+                for (int x = 0; x < ClientSize.Width; x += squareSize)
+                {
+                    var contourCase = GetContourCase(x, y);
+                    DrawCase(contourCase, x, y, e.Graphics);
+                }
+                
+            }
+        }
+
+        private Pen pen = Pens.Black;
+
+        private void DrawCase(int contourCase, int x, int y, Graphics g)
+        {
+
+            switch (contourCase)
+            {
+                case 0:
+                    break;
+                case 1:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    break;
+                case 2:
+                    g.DrawLine(pen, x + squareSize, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    break;
+                case 3:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + squareSize, y + halfSquareSize);
+                    break;
+                case 4:
+                    g.DrawLine(pen, x + halfSquareSize, y, x + squareSize, y + halfSquareSize);
+                    break;
+                case 5:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y);
+                    g.DrawLine(pen, x + halfSquareSize, y + squareSize, x + squareSize, y + halfSquareSize);
+                    break;
+                case 6:
+                    g.DrawLine(pen, x + halfSquareSize, y, x + halfSquareSize, y + squareSize);
+                    break;
+                case 7:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y);
+                    break;
+                case 8:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y);
+                    break;
+                case 9:
+                    g.DrawLine(pen, x + halfSquareSize, y, x + halfSquareSize, y + squareSize);
+                    break;
+                case 10:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    g.DrawLine(pen, x + halfSquareSize, y, x + squareSize, y + halfSquareSize);
+                    break;
+                case 11:
+                    g.DrawLine(pen, x + halfSquareSize, y, x + squareSize, y + halfSquareSize);
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    g.DrawLine(pen, x + squareSize, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    break;
+                case 14:
+                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    break;
+                case 15:
+                    break;
+            }
+        }
+
+        private float CircleFunc(float cx, float cy)
+        {
+            var l = x - cx;
+            var h = y - cy;
+            var dist = (float)Math.Sqrt(l * l + h * h);
+            return dist - r;
+        }
+
+        private bool TopLeftIn(int ix, int iy)
+        {
+            var dist = CircleFunc(ix, iy);
+
+            return dist <= 0;
+
+        }
+
+        private bool TopRightIn(int ix, int iy)
+        {
+            var dist = CircleFunc(ix + squareSize, iy);
+
+            return dist <= 0;
+
+        }
+
+        private bool BottomLeftIn(int ix, int iy)
+        {
+            var dist = CircleFunc(ix , iy + squareSize);
+
+            return dist <= 0;
+
+        }
+
+        private bool BottomRightIn(int ix, int iy)
+        {
+            var dist = CircleFunc(ix + squareSize, iy + squareSize);
+
+            return dist <= 0;
+
+        }
+
+        private int GetContourCase(int ix, int iy)
+        {
+            var bl = BottomLeftIn(ix, iy)? 1: 0;
+            var br = BottomRightIn(ix, iy)? 2: 0;
+            var tr = TopRightIn(ix, iy)? 4: 0;
+            var tl = TopLeftIn(ix, iy)? 8: 0;
+
+            return bl + br + tr + tl;
         }
 
         private int x = 0;
         private int y = 0;
+        private float r = 20.0f;
         private const int width = 40;
         private const int halfWidth = width / 2;
-        private Rectangle r = new Rectangle(0, 0, width, width);
+        private Rectangle rect = new Rectangle(0, 0, width, width);
+        private const int squareSize = 10;
+        private const int halfSquareSize = squareSize / 2;
+
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            r.X = e.X - halfWidth;
-            r.Y = e.Y - halfWidth;
+            x = e.X;
+            y = e.Y;
+            rect.X = e.X - halfWidth;
+            rect.Y = e.Y - halfWidth;
             Invalidate();
         }
     }
