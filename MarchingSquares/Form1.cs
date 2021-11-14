3ba1d4fa6981ecb233b25c12666f3fcc98858a39
liquidvapour <liquidvapour@gmail.com>
2021-11-14 13:43:35 +0000

    more refactoring

--- MarchingSquares/Form1.cs
+++ MarchingSquares/Form1.cs
@@ -6,6 +6,20 @@ namespace MarchingSquares
 {
     public partial class Form1 : Form
     {
+        private const float R = 63.0f;
+        private const int SquareSize = 8;
+        private const int HalfSquareSize = SquareSize / 2;
+        private const int BLIndex = 1;
+        private const int BRIndex = 2;
+        private const int TRIndex = 4;
+        private const int TLIndex = 8;
+
+        private readonly Func<float, float, float> _f;
+        
+        private int _mouseX;
+        private int _mouseY;
+
+
         public Form1()
         {
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
@@ -13,83 +27,83 @@ namespace MarchingSquares
             SetStyle(ControlStyles.UserPaint, true);
             SetStyle(ControlStyles.AllPaintingInWmPaint, true);
             InitializeComponent();
-            f = BubbleFunc;
+            _f = BubbleFunc;
         }
 
-        private Func<float, float, float> f;
 
         protected override void OnPaint(PaintEventArgs e)
         {
             //e.Graphics.DrawArc(Pens.Black, rect, 0.0f, 360.0f);
-            for (int y = 0; y < ClientSize.Height; y+=squareSize)
+            for (var y = 0; y < ClientSize.Height; y+=SquareSize)
             {
-                for (int x = 0; x < ClientSize.Width; x += squareSize)
+                for (var x = 0; x < ClientSize.Width; x += SquareSize)
                 {
-                    DrawCase(GetContourCase(x, y, 0.5f), x, y, e.Graphics, Pens.DarkRed);
-                    DrawCase(GetContourCase(x, y, 1), x, y, e.Graphics,Pens.Red);
-                    DrawCase(GetContourCase(x, y, 1.25f), x, y, e.Graphics, Pens.IndianRed);
+                    DrawCase(GetContourCase(x, y, 0.75f), x, y, e.Graphics, (Pens.IndianRed, Brushes.IndianRed));
+                    DrawCase(GetContourCase(x, y, 1), x, y, e.Graphics, (Pens.Red, Brushes.Red));
+                    DrawCase(GetContourCase(x, y, 1.5f), x, y, e.Graphics, (Pens.DarkRed, Brushes.DarkRed));
                 }
 
             }
         }
 
 
-        private void DrawCase(int contourCase, int x, int y, Graphics g, Pen pen)
+        private void DrawCase(int contourCase, int x, int y, Graphics g, (Pen Pen, Brush Brush) color)
         {
-
+            
             switch (contourCase)
             {
                 case 0:
+                    break;
                 case 15:
+                    g.FillRectangle(color.Brush, x, y, SquareSize, SquareSize);
                     break;
                 case 1:
                 case 14:
-                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    g.DrawLine(color.Pen, x, y + HalfSquareSize, x + HalfSquareSize, y + SquareSize);
                     break;
                 case 2:
                 case 13:
-                    g.DrawLine(pen, x + squareSize, y + halfSquareSize, x + halfSquareSize, y + squareSize);
+                    g.DrawLine(color.Pen, x + SquareSize, y + HalfSquareSize, x + HalfSquareSize, y + SquareSize);
                     break;
                 case 3:
                 case 12:
-                    g.DrawLine(pen, x, y + halfSquareSize, x + squareSize, y + halfSquareSize);
+                    g.DrawLine(color.Pen, x, y + HalfSquareSize, x + SquareSize, y + HalfSquareSize);
                     break;
                 case 4:
                 case 11:
-                    g.DrawLine(pen, x + halfSquareSize, y, x + squareSize, y + halfSquareSize);
+                    g.DrawLine(color.Pen, x + HalfSquareSize, y, x + SquareSize, y + HalfSquareSize);
                     break;
                 case 5:
-                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y);
-                    g.DrawLine(pen, x + halfSquareSize, y + squareSize, x + squareSize, y + halfSquareSize);
+                    g.DrawLine(color.Pen, x, y + HalfSquareSize, x + HalfSquareSize, y);
+                    g.DrawLine(color.Pen, x + HalfSquareSize, y + SquareSize, x + SquareSize, y + HalfSquareSize);
                     break;
                 case 6:
                 case 9:
-                    g.DrawLine(pen, x + halfSquareSize, y, x + halfSquareSize, y + squareSize);
+                    g.DrawLine(color.Pen, x + HalfSquareSize, y, x + HalfSquareSize, y + SquareSize);
                     break;
                 case 7:
                 case 8:
-                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y);
+                    g.DrawLine(color.Pen, x, y + HalfSquareSize, x + HalfSquareSize, y);
                     break;
                 case 10:
-                    g.DrawLine(pen, x, y + halfSquareSize, x + halfSquareSize, y + squareSize);
-                    g.DrawLine(pen, x + halfSquareSize, y, x + squareSize, y + halfSquareSize);
+                    g.DrawLine(color.Pen, x, y + HalfSquareSize, x + HalfSquareSize, y + SquareSize);
+                    g.DrawLine(color.Pen, x + HalfSquareSize, y, x + SquareSize, y + HalfSquareSize);
                     break;
             }
         }
 
         private float CircleFunc(float cx, float cy)
         {
-            var l = x - cx;
-            var h = y - cy;
-            var dist = r / (float)Math.Sqrt(l * l + h * h);
-            return dist;
+            var l = _mouseX - cx;
+            var h = _mouseY - cy;
+            return R / (float)Math.Sqrt(l * l + h * h);
         }
 
 
-        private int ax = 100;
-        private int ay = 100;
+        private const int OtherBlobX = 100;
+        private const int OtherBlobY = 100;
 
-        private float BubbleFunc(float cx, float cy) => GetDistOverRadius(cx, cy, x, y, r) + GetDistOverRadius(cx, cy, ax, ay, r);
+        private float BubbleFunc(float cx, float cy) => GetDistOverRadius(cx, cy, _mouseX, _mouseY, R) + GetDistOverRadius(cx, cy, OtherBlobX, OtherBlobY, R);
 
         private static float GetDistOverRadius(float cx, float cy, int i, int j, float radius)
         {
@@ -99,62 +113,30 @@ namespace MarchingSquares
         }
 
 
-        private bool TopLeftIn(int ix, int iy, float threshold)
-        {
-            var dist = f(ix, iy);
+        private static bool InsideThreshold(float dist, float threshold) => dist >= threshold;
 
-            return dist <= threshold;
+        private bool TopLeftIn(int ix, int iy, float threshold) => InsideThreshold(_f(ix, iy), threshold);
 
-        }
+        private bool TopRightIn(int ix, int iy, float threshold) => InsideThreshold(_f(ix + SquareSize, iy), threshold);
 
-        private bool TopRightIn(int ix, int iy, float threshold)
-        {
-            var dist = f(ix + squareSize, iy);
+        private bool BottomLeftIn(int ix, int iy, float threshold) => InsideThreshold(_f(ix , iy + SquareSize), threshold);
 
-            return dist <= threshold;
-
-        }
-
-        private bool BottomLeftIn(int ix, int iy, float threshold)
-        {
-            var dist = f(ix , iy + squareSize);
-
-            return dist <= threshold;
-
-        }
-
-        private bool BottomRightIn(int ix, int iy, float threshold)
-        {
-            var dist = f(ix + squareSize, iy + squareSize);
-
-            return dist <= threshold;
-
-        }
+        private bool BottomRightIn(int ix, int iy, float threshold) => InsideThreshold(_f(ix + SquareSize, iy + SquareSize), threshold);
 
         private int GetContourCase(int ix, int iy, float threshold)
         {
-            var bl = BottomLeftIn(ix, iy, threshold)? _blIndex: 0;
-            var br = BottomRightIn(ix, iy, threshold)? _brIndex: 0;
-            var tr = TopRightIn(ix, iy, threshold)? _trIndex: 0;
-            var tl = TopLeftIn(ix, iy, threshold)? _tlIndex: 0;
+            var bl = BottomLeftIn(ix, iy, threshold)? BLIndex: 0;
+            var br = BottomRightIn(ix, iy, threshold)? BRIndex: 0;
+            var tr = TopRightIn(ix, iy, threshold)? TRIndex: 0;
+            var tl = TopLeftIn(ix, iy, threshold)? TLIndex: 0;
 
             return bl + br + tr + tl;
         }
 
-        private int x = 0;
-        private int y = 0;
-        private float r = 63.0f;
-        private const int squareSize = 8;
-        private const int halfSquareSize = squareSize / 2;
-        private const int _blIndex = 1;
-        private const int _brIndex = 2;
-        private const int _trIndex = 4;
-        private const int _tlIndex = 8;
-
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            x = e.X;
-            y = e.Y;
+            _mouseX = e.X;
+            _mouseY = e.Y;
             Invalidate();
         }
     }
